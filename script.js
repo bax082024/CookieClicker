a5e3db4cb093df956584b520dc6b10183fd240ed
bax082024 <bax082024@gmail.com>
2024-10-13 09:58:56 +0200

    added const

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let totalCookies = 0;
 let cookiesPerClick = 1;
 let upgradeCost = 10;
 
+const cookieCountE1 = document.getElementById('cookieCount');
+const clickButton = document.getElementById('clickButton');
+const upgradeButton = document.getElementById('upgradeButton');
+const upgradeCostE1 = document.getElementById('upgradeCost');
